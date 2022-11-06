2918df5fd45d050c74cb2133801bbafce667acef
Michael MacFadden <michael@macfadden.org>
2022-11-06 12:15:01 -0600

    Removed tests we no longer use.

--- test-scripts/employee-id-test.ts
+++
@@ -1,8 +0,0 @@
-import {OpeEncryptor} from "../src/ope/OpeEncryptor";
-
-const key = "Sx7oMWG3l3mIWuMmlh3ZHYAwitZr6dp+MgZ2Nv8sk7E=";
-
-const encryptor = new OpeEncryptor(key);
-
-const enc001 = encryptor.encryptString("001");
-console.log(enc001);
